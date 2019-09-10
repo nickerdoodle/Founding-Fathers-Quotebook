82560d1f3d4e6f3ca1daf4793e861c6167da2e0c
Nick <nicholasmahe@gmail.com>
2019-09-10 08:38:42 -0600

    Initial Commit

---
+++ Founding Fathers Quote Book/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Founding Fathers Quote Book
+//
+//  Created by Nick Mahe on 9/10/19.
+//  Copyright © 2019 Nick Mahe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
