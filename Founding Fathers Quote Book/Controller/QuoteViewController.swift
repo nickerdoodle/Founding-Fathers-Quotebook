1bfa334d36fb5666626bdb314fb392bdc3191100
Nick <nicholasmahe@gmail.com>
2019-09-26 09:02:46 -0600

    Displaying topic names through controller

--- Founding Fathers Quote Book/Controller/QuoteViewController.swift
+++ Founding Fathers Quote Book/Controller/QuoteViewController.swift
@@ -10,17 +10,22 @@ import UIKit
 import WebKit
 
 class QuoteViewController : UIViewController {
+    
+    //Constants
+    private struct Storyboard{
+        static let quoteOfTheDayTitle = "Quote of the Day"
+    }
+    
     //Properties
     var currentQuoteIndex = 0
-    
+    var quotes: [Quote]!
+    var topic: String?
     //Outlets
     @IBOutlet weak var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        chooseQuoteOfTheDay()
-        updateUI()
-        
+        configure()
         
     }
     private func chooseQuoteOfTheDay(){
@@ -33,11 +38,33 @@ class QuoteViewController : UIViewController {
         }
     }
     
+    private func configure(){
+        
+        if let currentTopic = topic{
+            currentQuoteIndex = 0
+            quotes = QuoteDeck.sharedInstance.quotes(for: currentTopic)
+            title = "\(currentTopic.capitalized) \(currentQuoteIndex + 1) of \(quotes.count)"
+        }
+        
+        else{
+            quotes = QuoteDeck.sharedInstance.quotes
+            chooseQuoteOfTheDay()
+            title = Storyboard.quoteOfTheDayTitle
+        }
+        
+        updateUI()
+    }
+    
     private func updateUI(){
-        let currentQuote = QuoteDeck.sharedInstance.quotes[currentQuoteIndex]
+        let currentQuote = quotes[currentQuoteIndex]
         webView.loadHTMLString(currentQuote.html, baseURL : nil)
     }
     @IBAction func exitModalScene(_ segue: UIStoryboardSegue){
         //In this case, there is nothing to do, but we need a target
+        topic = nil
+        configure()
+    }
+    @IBAction func showTopicQuotes(_ segue: UIStoryboardSegue){
+        configure()
     }
 }
--- Founding Fathers Quote Book/Controller/TopicsViewController.swift
+++ Founding Fathers Quote Book/Controller/TopicsViewController.swift
@@ -11,14 +11,19 @@ import UIKit
 class TopicsViewController: UITableViewController{
     // Constant
     private struct Storyboard{
+        static let showQuoteSegueIdentifier = "ShowQuote"
         static let topicCellIdentifier = "TopicCell"
     }
     
     //Properties
-    
+    var selectedTopic: String?
     
     //View Controller Lifecycle
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let quoteViewController = segue.destination as? QuoteViewController {
+            quoteViewController.topic = selectedTopic
+        }
+    }
     
     //Table view data source
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
@@ -34,6 +39,7 @@ class TopicsViewController: UITableViewController{
     //Table view delegate
     
     override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        
+        selectedTopic = QuoteDeck.sharedInstance.tagSet[indexPath.row]
+        performSegue(withIdentifier: Storyboard.showQuoteSegueIdentifier, sender: self)
     }
 }
